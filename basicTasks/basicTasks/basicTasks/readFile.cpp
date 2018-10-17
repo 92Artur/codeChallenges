3d8d0a36b75097252e10db2abdc2a9afed32a0e0
Artur <artur.marks@uni-muenster.de>
2018-10-17 16:06:16 +0200

    add folder for basic tasks

---
+++ basicTasks/basicTasks/basicTasks/readFile.cpp
@@ -0,0 +1,12 @@
+﻿// basicTasks.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "readFile.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
