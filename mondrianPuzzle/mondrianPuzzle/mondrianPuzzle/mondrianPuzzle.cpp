729d8b8751027013402c6b159b01944400a812c6
Artur <artur.marks@uni-muenster.de>
2018-10-17 15:49:20 +0200

    inital commit

---
+++ mondrianPuzzle/mondrianPuzzle/mondrianPuzzle/mondrianPuzzle.cpp
@@ -0,0 +1,12 @@
+﻿// mondrianPuzzle.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "mondrianPuzzle.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
